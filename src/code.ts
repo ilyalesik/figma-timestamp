349801e8f40341a971382cd2853804acd2db3cde
Ilia Lesik <ilialesik@gmail.com>
2019-08-07 23:34:12 +0300

    Implement "Update all timestamps" command

--- src/code.ts
+++ src/code.ts
@@ -1,22 +1,77 @@
 import {format} from 'date-fns'
 
-figma.showUI(__html__, {
-    height: 156
-});
+let timer = undefined;
 
-const font = {family: "Roboto", style: "Regular"};
+if (figma.command === "add-new") {
+    figma.showUI(__html__, {
+        height: 156
+    });
 
-figma.ui.onmessage = async msg => {
+    const font = {family: "Roboto", style: "Regular"};
 
-    if (msg.type === 'create-clock') {
-        const node = figma.createText();
-        await figma.loadFontAsync(font);
-        node.fontName = font;
-        node.characters = format(new Date(), msg.format);
-        figma.currentPage.appendChild(node);
-        figma.currentPage.selection = [node];
-        figma.viewport.scrollAndZoomIntoView([node]);
-    }
+    figma.ui.onmessage = async msg => {
+
+        if (msg.type === 'create-clock') {
+            const node = figma.createText();
+            await figma.loadFontAsync(font);
+            node.fontName = font;
+            node.characters = format(new Date(), msg.format);
+            node.setPluginData("timestampFormat", msg.format);
+            figma.currentPage.appendChild(node);
+            figma.currentPage.selection = [node];
+            figma.viewport.scrollAndZoomIntoView([node]);
+        }
+
+        figma.closePlugin();
+    };
+} else if (figma.command === "update-all") {
+    const currentDate = new Date();
+    searchFor((node) => !!node.getPluginData("timestampFormat"), results => results.forEach(async (node) => {
+        if (node.fontName instanceof Object) {
+            await figma.loadFontAsync(node.fontName);
+            node.characters = format(currentDate, node.getPluginData("timestampFormat"));
+        }
+    }));
 
     figma.closePlugin();
-};
+
+    // This is a generator that recursively produces
+    // all the nodes in subtree starting at the given node
+    function* walkTree(node) {
+        yield node;
+        let children = node.children;
+        if (children) {
+            for (let child of children) {
+                yield* walkTree(child)
+            }
+        }
+    }
+
+    function searchFor(predicat: (node: BaseNodeMixin) => boolean, handler: (results: Array<TextNode>) => any) {
+        let walker = walkTree(figma.currentPage)
+
+        function processOnce() {
+            let results = [];
+            let count = 0;
+            let done = true;
+            let res
+            while (!(res = walker.next()).done) {
+                let node = res.value
+                if (predicat(node)) {
+                    results.push(node);
+                }
+                if (++count === 1000) {
+                    done = false
+                    timer = setTimeout(processOnce, 20)
+                    break
+                }
+            }
+
+            handler(results);
+        }
+
+        processOnce()
+    }
+}
+
+
